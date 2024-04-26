689657e1ace2d44202f92435ed0cfe3aaea2946a
Disney <ivan36samsonov@gmail.com>
2024-04-26 08:40:06 +0300

    alembic, cors and environment variable

--- app/database.py
+++ app/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+# добавляем +psycopg2 он в сравнение чем без него, отправляет запросы к бд быстрее
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{settings.DB_USER}:{settings.DB_PASS}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from sqladmin import Admin
+from fastapi.middleware.cors import CORSMiddleware
 from . import models, schemas, utils
 from .database import engine, get_db
 from admin import views, auth as admin_auth
@@ -11,11 +12,23 @@ from .config import settings
 
 
 # orm
+# теперь когда у нас в проекте есть alembic у нас нет необходимости в этой строчке которая создает таблицы по sqlalchemy моделям, если таблиц с такими названиями не существует
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title = 'Spider-verse'
 )
+
+origins = ['https://www.google.com']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # важно назвать не templates и не statics, чтобы не было конфликтов имен с sqladmin
 template = Jinja2Templates(directory="template")
